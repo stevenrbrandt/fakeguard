fcc0909dfb28e01712a71ae3a8cbde46e2868727
Steven R. Brandt <sbrandt@cct.lsu.edu>
2022-11-07 13:45:35 -0600

    Cleaning up

--- GuardTask.java
+++ GuardTask.java
@@ -8,7 +8,6 @@ public class GuardTask {
     final static AtomicInteger nextId = new AtomicInteger(-1);
     final int id = nextId.getAndIncrement();
     public String toString() { return "gt["+id+","+guard+","+next+"]"; }
-    final static AtomicInteger activeCount = new AtomicInteger(-1);
     final Guard guard;
     final static GuardTask DONE = new GuardTask(null,()->{
         assert false : "DONE should not be executed";
@@ -26,14 +25,12 @@ public class GuardTask {
         guard = g;
         cleanup = false;
         this.guards_held = guards_held;
-        activeCount.getAndIncrement();
     }
     public GuardTask(Guard g,Runnable r,TreeSet<Guard> guards_held) {
         guard = g;
         this.r = r;
         cleanup = true;
         this.guards_held = guards_held;
-        activeCount.getAndIncrement();
     }
     public void setRun(Runnable r) {
         assert this.r == null;
@@ -43,10 +40,12 @@ public class GuardTask {
         assert this != DONE : "DONE should not be executed";
         int id = ThreadID.get();
         assert guard.locked.compareAndSet(false,true) : String.format("%s %d %d",this,ThreadID.get(),guards_held.size());
-        if(cleanup) for(Guard g : guards_held) assert g.id <= guard.id : "Not last";
-        if(cleanup) GUARDS_HELD.set(guards_held);
+        if(cleanup) {
+            GUARDS_HELD.set(guards_held);
+            for(Guard g : guards_held)
+                assert g.locked.get();
+        }
         Run.run(r);
-        activeCount.getAndDecrement();
     }
     public void run() {
         run_();
